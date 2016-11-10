d719429f6d7af26920d8657efbcb82fa8ccda19d
Matthew King <thewmking@gmail.com>
2016-11-10 11:56:27 -0500

    now works with capital letters

--- cipher.rb
+++ cipher.rb
@@ -5,11 +5,16 @@ puts "what is your shift factor?"
 shift_factor = gets.chomp
 shift = shift_factor.to_i
 
-
 def caesar_cipher(message, shift)
   alphabet = Array('a'..'z')
-  encrypter = Hash[alphabet.zip(alphabet.rotate(shift))]
-  message.chars.map { |c| encrypter.fetch(c, " ")  }
+  non_caps = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+  alphabet = Array('A'..'Z')
+  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+  encrypter = non_caps.merge(caps)
+
+  message.chars.map { |c| encrypter.fetch(c, c)}
 end
 
 puts caesar_cipher(message, shift).join
